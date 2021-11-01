635eade5c6d3552d7a0933005897132c8e7e6956
Lyutink <krivenko.lyuda@gmail.com>
2021-11-01 12:59:23 +0300

    Update HW-08_3_js_notReady
    ready   item 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import  throttle  from 'lodash.throttle';
+const iframeRef = document.querySelector('iframe');
+const player = new Vimeo.Player(iframeRef);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
+const throttledCurrentTime = throttle(saveCurrentTime, 1000);
+
+function saveCurrentTime() {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+    
+player.on('timeupdate', throttledCurrentTime);
+
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+player.setCurrentTime(currentTime).catch(function (error) {
+        console.log(error);
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const feedbeckFormRef = document.querySelector('.feedback-form');
+let selectedFeedbeckForm = {};
 
+initFeedbeckForm();
+
+feedbeckFormRef.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(feedbeckFormRef);
+    formData.forEach((value, name) => console.log(value, name));
+    localStorage.removeItem('feedback-form-state');
+    
+})
+
+feedbeckFormRef.addEventListener('input', event => {
+    selectedFeedbeckForm[event.target.name] = event.target.value;
+    console.log(selectedFeedbeckForm);
+    localStorage.setItem('feedback-form-state', JSON.stringify(selectedFeedbeckForm));
+})
+
+function initFeedbeckForm() {
+    let persistedForm = localStorage.getItem('feedback-form-state');
+    if (persistedForm) {
+        persistedForm = JSON.parse(persistedForm);
+        Object.entries(persistedForm).forEach(([name, value]) => {
+            selectedFeedbeckForm[name] = value;
+            feedbeckFormRef.elements[name].value = value;
+        })   
+    } 
