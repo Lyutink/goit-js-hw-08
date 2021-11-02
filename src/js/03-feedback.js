74aa7c3cb24814f197d2c0f3a79130294367c334
Lyutink <krivenko.lyuda@gmail.com>
2021-11-02 12:58:32 +0300

    Update HW-08.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
 const feedbeckFormRef = document.querySelector('.feedback-form');
 let selectedFeedbeckForm = {};
 
 initFeedbeckForm();
 
+feedbeckFormRef.addEventListener('input', throttle(saveLocalStorage, 500));
+
 feedbeckFormRef.addEventListener('submit', event => {
     event.preventDefault();
-    const formData = new FormData(feedbeckFormRef);
-    formData.forEach((value, name) => console.log(value, name));
-    localStorage.removeItem('feedback-form-state');
+    console.log(selectedFeedbeckForm);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    feedbeckFormRef.reset();
+    selectedFeedbeckForm = {};
     
 })
 
-feedbeckFormRef.addEventListener('input', event => {
+function saveLocalStorage(event) {
     selectedFeedbeckForm[event.target.name] = event.target.value;
     console.log(selectedFeedbeckForm);
     localStorage.setItem('feedback-form-state', JSON.stringify(selectedFeedbeckForm));
-})
 
+}
 function initFeedbeckForm() {
-    let persistedForm = localStorage.getItem('feedback-form-state');
+    console.log(selectedFeedbeckForm);
+    let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
     if (persistedForm) {
         persistedForm = JSON.parse(persistedForm);
         Object.entries(persistedForm).forEach(([name, value]) => {
             selectedFeedbeckForm[name] = value;
             feedbeckFormRef.elements[name].value = value;
-        })   
-    } 
+        })
+    }
+}
